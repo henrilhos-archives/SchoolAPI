c62cedaa4cdb9dbebe5f84830c776cb5e36c4813
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-06-10 21:45:01 -0300

    :heavy_plus_sign: CORS adicionado ao projeto

--- lib/app.ts
+++ lib/app.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from 'body-parser'
 import * as express from 'express'
 import * as mongoose from 'mongoose'
+import * as cors from 'cors'
 import { Config } from './config'
 import { StudentRoutes } from './routes/student.route'
 
@@ -19,6 +20,7 @@ class App {
   private appConfig(): void {
     this.app.use(bodyParser.json())
     this.app.use(bodyParser.urlencoded({ extended: false }))
+    this.app.use(cors())
   }
 
   private mongoSetup(): void {
