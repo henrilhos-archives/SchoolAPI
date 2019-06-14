0052205fbe5bee06440e698b8a2e05098045587b
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-06-14 10:06:02 -0300

    :construction: Exemplo de agregação

--- res/mongo/aggregation.js
+++ res/mongo/aggregation.js
@@ -28,6 +28,44 @@ db.getCollection('students').aggregate(
         preserveNullAndEmptyArrays: false, // optional
       },
     },
+
+    // Stage 3
+    {
+      $project: {
+        name: 1,
+        demographics: 1,
+        addresses: 1,
+        studentUsername: 1,
+        email: 1,
+        currentSemester: 1,
+        schoolSubject: {
+          title: '$schoolSubjects.title',
+          semester: '$schoolSubjects.semester',
+          teacher: '$schoolSubjects.teacher',
+          grades: '$schoolSubjects.grades',
+          average: '$schoolSubjects.grades.grade',
+        },
+      },
+    },
+
+    // Stage 4
+    {
+      $project: {
+        name: 1,
+        demographics: 1,
+        addresses: 1,
+        studentUsername: 1,
+        email: 1,
+        currentSemester: 1,
+        schoolSubject: {
+          title: 1,
+          semester: 1,
+          teacher: 1,
+          grades: 1,
+          average: { $avg: '$schoolSubject.average' },
+        },
+      },
+    },
   ],
 
   // Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
