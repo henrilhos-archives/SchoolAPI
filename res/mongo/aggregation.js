faf1c065916ab470d7a0c075943ea348555083a0
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-06-16 18:08:38 -0300

    :construction: Buscando matérias reprovadas

--- res/mongo/aggregation.js
+++ res/mongo/aggregation.js
@@ -1,72 +1,92 @@
-db.getCollection('students').aggregate(
-  // Pipeline
-  [
-    // Stage 1
-    {
-      $project: {
-        name: 1,
-        demographics: 1,
-        addresses: 1,
-        studentUsername: 1,
-        email: 1,
-        currentSemester: 1,
-        schoolSubjects: {
-          $filter: {
-            input: '$schoolSubjects',
-            as: 'schoolSubject',
-            cond: { $eq: ['$$schoolSubject.semester', '$currentSemester'] },
-          },
-        },
-      },
-    },
+db.getCollection("students").aggregate(
 
-    // Stage 2
-    {
-      $unwind: {
-        path: '$schoolSubjects',
-        includeArrayIndex: 'arrayIndex', // optional
-        preserveNullAndEmptyArrays: false, // optional
-      },
-    },
+	// Pipeline
+	[
+		// Stage 1
+		{
+			$project: {
+				name: 1,
+				demographics: 1,
+				addresses: 1,
+				studentUsername: 1,
+				email: 1,
+				currentSemester: 1,
+				schoolSubjects: {
+					$filter: {
+						input: '$schoolSubjects',
+						as: 'schoolSubject',
+						cond: { $eq: ['$$schoolSubject.semester', '$currentSemester'] }
+					}
+				}
+			}
+		},
 
-    // Stage 3
-    {
-      $project: {
-        name: 1,
-        demographics: 1,
-        addresses: 1,
-        studentUsername: 1,
-        email: 1,
-        currentSemester: 1,
-        schoolSubject: {
-          title: '$schoolSubjects.title',
-          semester: '$schoolSubjects.semester',
-          teacher: '$schoolSubjects.teacher',
-          grades: '$schoolSubjects.grades',
-          average: '$schoolSubjects.grades.grade',
-        },
-      },
-    },
+		// Stage 2
+		{
+			$unwind: {
+				path: '$schoolSubjects',
+				includeArrayIndex: 'arrayIndex',
+				preserveNullAndEmptyArrays: false
+			}
+		},
 
-    // Stage 4
-    {
-      $project: {
-        name: 1,
-        demographics: 1,
-        addresses: 1,
-        studentUsername: 1,
-        email: 1,
-        currentSemester: 1,
-        schoolSubject: {
-          title: 1,
-          semester: 1,
-          teacher: 1,
-          grades: 1,
-          average: { $avg: '$schoolSubject.average' },
-        },
-      },
-    },
-  ],
+		// Stage 3
+		{
+			$project: {
+				name: 1,
+				demographics: 1,
+				addresses: 1,
+				studentUsername: 1,
+				email: 1,
+				currentSemester: 1,
+				schoolSubject: {
+					title: '$schoolSubjects.title',
+					semester: '$schoolSubjects.semester',
+					teacher: '$schoolSubjects.teacher',
+					grades: '$schoolSubjects.grades',
+					average: { $avg: '$schoolSubjects.grades.grade' }
+				}
+			}
+		},
 
-  // Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
-)
+		// Stage 4
+		{
+			$group: {
+				_id : {
+					_id: '$_id',
+					name: '$name',
+					demographics: '$demographics',
+					addresses: '$addresses',
+					studentUsername: '$studentUsername',
+					email: '$email',
+					currentSemester: '$currentSemester'
+				},
+				schoolSubjects: { $push: '$schoolSubject' }
+			}
+		},
+
+		// Stage 5
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+				demographics: '$_id.demographics',
+				addresses: '$_id.addresses',
+				studentUsername: '$_id.studentUsername',
+				email: '$_id.email',
+				currentSemester: '$_id.currentSemester',
+				schoolSubjects: {
+					$filter: {
+						input: '$schoolSubjects',
+						as: 'schoolSubject',
+						cond: { $lt: ['$$schoolSubject.average', 6] }
+					}
+				}
+			}
+		},
+
+	]
+
+	// Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
+
+);
